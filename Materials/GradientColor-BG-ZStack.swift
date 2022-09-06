4ce79eca848b62024fe06983eb96f3d9971534bc
Omar Khattab <65384838+omr1k@users.noreply.github.com>
2022-09-06 13:16:36 +0300

    Delete GradientColor-BG-ZStack.swift

--- Materials/GradientColor-BG-ZStack.swift
+++
@@ -1,9 +0,0 @@
-ZStack{
-                Rectangle()
-                        .fill(LinearGradient(
-                          gradient: gradient,
-                          startPoint: .init(x: 0.00, y: 0.50),
-                          endPoint: .init(x: 1.00, y: 0.50)
-                        ))
-                      .edgesIgnoringSafeArea(.all)
-             }
