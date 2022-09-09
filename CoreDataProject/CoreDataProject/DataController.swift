dce2bc928f0986f86b6d74fb23d003f7ab14dac7
Omar Khattab <omr11kk@gmail.com>
2022-09-09 04:59:06 +0300

    update bookworm app

---
+++ CoreDataProject/CoreDataProject/DataController.swift
@@ -0,0 +1,24 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Omar Khattab on 05/09/2022.
+//
+
+import CoreData
+import Foundation
+
+
+class DataController : ObservableObject{
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init(){
+        container.loadPersistentStores{description,error in
+            if let error = error {
+                print("Core date failed to load \(error.localizedDescription)")
+            }
+            
+        }
+    }
+    
+}
