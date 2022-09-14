baa5d7836aaea7261a5d813d08d19fe0544c7125
Omar Khattab <omr11kk@gmail.com>
2022-09-14 21:44:41 +0300

    Instafilter app initial commit

---
+++ Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Omar Khattab on 13/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Omar Khattab on 13/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
