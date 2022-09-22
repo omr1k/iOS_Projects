b4f5303217537adb02f557e340a6f21b4b7c6dc2
Omar Khattab <omr11kk@gmail.com>
2022-09-22 04:42:26 +0300

    BucketList App initial commit

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Omar Khattab on 21/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
