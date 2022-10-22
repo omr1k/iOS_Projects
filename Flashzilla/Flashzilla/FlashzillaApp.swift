f5f76e876ec6c1ec6e4195c9e32d1a99333a6ddc
Omar Khattab <omr11kk@gmail.com>
2022-10-22 18:11:59 +0300

    Flashzilla app initial commit

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Omar Khattab on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
